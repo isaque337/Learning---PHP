7818c944cf9a3a69821f3247ceb35c3e3a7c6097
Isaque/delta <isaque.337dd@gmail.com>
2021-09-23 17:13:24 -0300

    space

--- Files/Comp null cao.php
+++ Files/Comp null cao.php
@@ -2,17 +2,12 @@
 $idade = '45';
 $idade2 = '20';
 
-
-
-
 if($idade>18) {
     echo "É maior.";
 }
 else {
     echo "É menor.";
 }
-
-
 
 $total = isset($idade2) ? $idade2 : 'Não tem';
 echo $total;
