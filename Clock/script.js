59de2c04602b21777d6988c88bb770b1bc093ea7
Isaque Almeida <isaque.337dd@gmail.com>
2021-10-05 17:51:54 -0300

    Alter simply

---
+++ Clock/script.js
@@ -0,0 +1,4 @@
+function mostrarAlerta() {
+
+    alert('Você clicou');
+}
--- Files/Array call.php
+++ Files/Array call.php
@@ -7,10 +7,15 @@ $lista = [
         'agilidade' => 80,
         'destreza' => 50
     ],
+    'fome'=> 200,
     'vida' => 1000,
-    'mana' => 928
+    'comida' => 928
 ];
 
 echo "NOME:" . $lista['nome'] . "<br/>";
+if ($lista['nome'] == "Isaque"){
+    echo "Your life is: " . $lista['vida']. "<br>";
+}
 echo "FORÇA:" . $lista['atributos']['força'] . "<br/>";
-echo "VIDA:" . $lista['vida'];
+echo "VIDA:" . $lista['vida'] . "<br>";
+echo "A fome é: ". $lista['fome'];
