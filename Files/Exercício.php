3f98a455c7c8251e8da11c540dec5ee849f1d3b9
Isaque Almeida <isaque.337dd@gmail.com>
2021-10-07 16:58:30 -0300

    Sucess in exercise!

--- Files/Exercício.php
+++ Files/Exercício.php
@@ -2,10 +2,10 @@
 
 echo "<h2>Impressão de 10 elementos com loop em notação big ô</h2>";
 
-for($i=1; $i<10; $i++){
-    echo "-<br>";
-    for($j=1; $j<10;$j++){
+for($i=1; $i<=10; $i++){
+    echo "-";
+    for($j=1; $j<10; $j++){
         echo "-";
     }   
-
+    echo "<br>";
 };
