ef6abe863d7c1ab33fcf9d43f8c50a73a3aef97c
Isaque Almeida <isaque.337dd@gmail.com>
2021-10-07 17:25:12 -0300

    Read!

--- Files/Exercise2.php
+++ Files/Exercise2.php
@@ -3,7 +3,8 @@
 echo "<h2>Impressão de 20^2 elementos crescentes com notação big ô</h2>";
 
 for($i=1;$i<=20;$i++){
-    for($j=1;$j<=20;$j++){
-
+    for($j=1;$j<=$i;$j++){
+        echo " - ";
     }
+    echo "<br>";
 }
