4097d13a1cc4db2af73e710413d25104e893c16c
Isaque Almeida <isaque.337dd@gmail.com>
2021-10-26 16:42:11 -0300

    Class

--- Serviço/cliente/cadastrar.php
+++ Serviço/cliente/cadastrar.php
@@ -1,5 +1,10 @@
 <?php
+require __DIR__ . './../vendor/autoload.php';
 
+//Validação do formulário 
 
-
-?>
+if(isset($_POST['nome'],$_POST['cpf'],$_POST['status'])){
+    echo 'Cadastrar';
+}else{
+    echo'Não cadastrou';
+}
