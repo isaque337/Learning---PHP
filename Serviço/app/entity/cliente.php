7788977cffb5b7de9e600b7332468751551d9fac
Isaque Almeida <isaque.337dd@gmail.com>
2021-10-26 17:06:08 -0300

    entity

---
+++ Serviço/app/entity/cliente.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace app\entity;
+
+Class nome{
+    /**
+     * Identificador do cliente
+     * @var integer
+     */
+    public $id;
+
+    /**
+     * Nome do cliente
+     * @var string
+     */
+    public $nome;
+
+    /**
+     * Nome do cpf
+     * @var integer
+     */
+    public $cpf;
+
+    /**
+     * Status (ativo ou inativo)
+     * @var string
+     */
+}
